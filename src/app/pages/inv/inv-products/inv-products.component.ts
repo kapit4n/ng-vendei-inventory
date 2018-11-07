b06b41904f2f1c9545d8f1d40a0b75e19c67307d
lartl <luis.arce22@gmail.com>
2018-11-07 08:10:03 -0400

    Adding boostrap library and inv product component

---
+++ src/app/pages/inv/inv-products/inv-products.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inv-products',
+  templateUrl: './inv-products.component.html',
+  styleUrls: ['./inv-products.component.css']
+})
+export class InvProductsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
