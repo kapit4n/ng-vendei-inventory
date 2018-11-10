cf0fa931c153940dbe7574045300a86bb0dea710
lartl <luis.arce22@gmail.com>
2018-11-10 06:53:51 -0400

    Add inventory item with model

--- src/app/pages/inv/inv-products-inv/inv-products-inv.component.ts
+++ src/app/pages/inv/inv-products-inv/inv-products-inv.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { Router, ActivatedRoute } from "@angular/router";
 import { IProductsService } from "../../../services/inv/i-products.service";
 import { IProductsInvService } from "../../../services/inv/i-products-inv.service";
+import { NgbModal, ModalDismissReasons } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: "app-inv-products-inv",
@@ -12,9 +13,11 @@ export class InvProductsInvComponent implements OnInit {
   invInfo: any = {};
   productInvs: any[] = [];
   invItemInfo: any = {};
+  closeResult: string;
 
   constructor(
     private route: ActivatedRoute,
+    private modalService: NgbModal,
     private productSvc: IProductsService,
     private productInvSvc: IProductsInvService
   ) {}
@@ -28,18 +31,36 @@ export class InvProductsInvComponent implements OnInit {
       this.loadProductInvs(id);
     }
   }
-  
+
+  open(content) {
+    this.modalService.open(content).result.then(
+      result => {
+        this.closeResult = `Closed with: ${result}`;
+      },
+      reason => {
+        this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+      }
+    );
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return "by pressing ESC";
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return "by clicking on a backdrop";
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+
   loadProductInvs(id: string) {
     this.productInvSvc.getByProductId(id).subscribe(invs => {
       this.productInvs = invs;
     });
-
   }
 
-  addInventory() {
-    this.invItemInfo.quantity = 10;
-    this.invItemInfo.price = 10;
-    this.invItemInfo.totalPrice = 100;
+  saveInvItem() {
+    this.invItemInfo.totalPrice = this.invItemInfo.quantity * this.invItemInfo.price;
     this.invItemInfo.productId = this.invInfo.id;
 
     this.productInvSvc.save(this.invItemInfo).subscribe(product => {
